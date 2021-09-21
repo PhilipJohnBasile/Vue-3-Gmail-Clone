341d348df4b7bf350d213fd743e0f833fcf17cdd
Philip John Basile <PhilipJohnBasile@gmail.com>
2021-09-20 20:21:54 -0400

    In Progress Gmail Vue Clone

---
+++ src/components/ModalView.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class='modal'>
+    <div class='overlay' @click="emit('closeModal')"></div>
+    <div class='modal-card'>
+      <slot/>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  setup(props, {emit}) {
+    let onKeyDown = (event) => {
+      console.log(event.key)
+    }
+    window.addEventListener('keydown', onKeyDown)
+    return {
+      emit
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
